0447e238ad6b248a99cc900b94c3c5691352bdce
Matthew Ticciati <mticciati@gmail.com>
2024-04-05 16:54:34 -0700

    add edit invoice path with dynamic route segment, updateInvoice action wired to invoice edit form - TODO handle errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -38,3 +38,28 @@ export async function createInvoice(formData: FormData): Promise<void> {
 
 }
 
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+
+	const { customerId, amount, status } = UpdateInvoice.parse({
+		customerId: formData.get('customerId'),
+		amount: formData.get('amount'),
+		status: formData.get('status'),
+	});
+
+	//TODO - handle errors
+	
+	const amountInCents = amount * 100;
+
+	sql`
+		UPDATE invoices 
+		SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+		WHERE id = ${id}
+	`;
+
+	revalidatePath('/dashboard/invoices');
+	redirect('/dashboard/invoices');
+}
+
